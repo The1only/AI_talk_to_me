0e7d923341a66302fb8c01dec81d0d33e6c11420
The1Only <teni@9tek.no>
2024-03-17 13:03:03 +0100

    Initial version...

--- README.md
+++ README.md
@@ -1,2 +1,120 @@
-# AI_talk_to_me
-Have a discussion with you're local AI, while you walk around in the room, no buttons needed...
+## Getting Started
+
+This section will guide you through preparing your local machine for running the LM-Studio-Voice-Conversation project, including installing prerequisites, setting up the Python environment, and running the project.
+It is also possible to run this software using the llama_cpp.server directly from a command prompt. This is easier on systems that LM Studio does not support.
+
+### Prerequisites
+
+Before you begin, ensure you have the following installed:
+
+- **Miniconda or Anaconda**: Download and install from [Anaconda's official site](https://www.anaconda.com/).
+- **LM Studio**: Access and information available at [LM Studio's website](https://lmstudio.ai/).
+
+OR, if you are running the server directly from a command prompt:
+
+- **llama_cpp.server**: pip install llama_cpp.server
+
+I like to use Miniconda as I always work in the command shell. 
+
+### Setting Up Your Python Environment
+
+Follow these steps to prepare your environment:
+
+1. **Install conda**: Follow the Miniconda/Anaconda installation instructions for your operating system available on the Anaconda website.
+
+2. **Create a New Conda Environment**:
+   ```bash
+   conda create -n myenv python=3.9.18
+
+   Replace `myenv` with a name of your choice for the environment.
+
+3. **Activate the Environment**:
+   ```bash
+   conda activate myenv
+   ```
+
+### Clone the Repository
+Get the project code by cloning the Voice-Conversation repository:
+```bash
+git clone https://github.com/The1only/AI_talk_to_me.git
+```
+
+4. **Install Required Packages**:
+   Navigate to the cloned directory and install the necessary packages:
+   ```bash
+   pip install -r requirements.txt
+   pip install git+https://github.com/openai/whisper.git
+   ```
+
+### Running the Project
+
+5. Before running the project you must have a LLM running on your local computer or on a remote system.
+
+   LM Studio on a local computer:
+      Start LM Studio, and download an LLM model. I use "mistral-7b-instruct-v0.2.Q4_K_S.gguf" as this works very nicely. 
+      Start the LLM module from within the LM Studio...
+
+   LM Studio on a remote system:
+      Start LM Studio, and download an LLM model. I use "mistral-7b-instruct-v0.2.Q4_K_S.gguf" as this works very nicely. 
+      Start the LLM module from within the LM Studio...
+      Get the system IP address and enter this in the speak_only.py file.
+
+   OR, if you are running the server directly from a command prompt:
+      wget the LLM model, in my case the "mistral-7b-instruct-v0.2.Q4_K_S.gguf", and put it in the "models" folder. 
+      To start enter:
+      python3 -m llama_cpp.server --model models/mistral-7b-instruct-v0.2.Q4_K_S.gguf --port 1234 --host 0.0.0.0 
+      or you can experiment with this when you are using GPUs:
+      python3 -m llama_cpp.server --model models/mistral-7b-instruct-v0.2.Q4_K_S.gguf --n_gpu_layers 1000 --port 1234 --host 0.0.0.0 --n_ctx 4096
+
+- **LLM Python Script (`speak_only.py`)**: Main script for the language model.
+
+6. To run the script, execute this command in your terminal:
+   ```bash
+   python speak_only.py
+   ```
+
+7. Exit
+
+   Hitt ´q´ to exit the program.
+
+8. Running
+
+   When the program is started, the system listens for a second for the ambient noise in the room. This is then used for speech threshold. 
+
+   The computer is named 'Mary', and she will only answer questions that in so way contain her name. 
+   You can talk to others in the room, but as soon as her name is mentioned she will reply. 
+   Eg: "Who was the third American president, Mary" or "Mary, who was the first American president" or "Do you know Marry, who was the second American president?"
+   these are all legal questions, but she will only answer if you say "Mary" in the question.
+
+   'Mary' listens for pauses in the talk, as humans, so when you have asked a question and are silent, she will start processing the question. 
+
+9. Tuning
+
+   There are several parameters that might need tuning:
+
+      # Get available output devices
+      mixer.init()
+      print("Outputs:", devices.audio.get_audio_device_names(False))
+      mixer.quit()
+      mixer.init(devicename = 'MacBook Air Speakers')
+
+      You might need to change this parameter according to the printout or comment out all these lines to get the default output device.
+
+      # Set the voice threshold
+      print(str((x+old_x)/2.0)+" : " + str(rms*2)) 
+      if ((x+old_x)/2.0) > (rms*2):
+
+      By enabling the print line you can see how strong you're voice is compared to the background, and then set the scaling factor accordingly.
+      In a quiet environment, I have found 10 to be OK, in a noisy environment I have found 2 to be OK.
+
+      # Set the silence threshold
+      Check if rms is low enough to stop recording...
+      if x < rms*2:
+
+      You might need to adjust this calar according to your ambient noise.
+
+## Development Environment Setup
+
+For detailed instructions on setting up and using Visual Studio Code with this project, please see [VSCode Instructions](VSCodeSetup.md).
+
+
---
+++ speak_only.py
@@ -0,0 +1,330 @@
+#------------------------------------------------
+# Step 1: Import necessary libraries and modules
+import warnings
+import pyaudio
+import wave
+import whisper
+import openai
+from openai import OpenAI
+from pynput import keyboard
+import os
+import pyttsx3
+import tkinter as tk
+from tkinter import simpledialog
+import time
+import struct
+import math
+from pygame import mixer, _sdl2 as devices
+
+print("All libraries and modules have been imported successfully... ")      
+print("Booting up the system...")
+
+# Get available output devices
+mixer.init()
+print("Outputs:", devices.audio.get_audio_device_names(False))
+mixer.quit()
+mixer.init(devicename = 'MacBook Air Speakers')
+
+#------------------------------------------------
+# Point to the local server
+#client = OpenAI(base_url="http://10.19.0.112:1234/v1", api_key="not-needed")
+#client = OpenAI(base_url="http://10.19.0.66:1234/v1", api_key="not-needed")
+client = OpenAI(base_url="http://localhost:1234/v1", api_key="not-needed")
+
+#------------------------------------------------
+# Step 2: Initialize Text-to-Speech engine (Windows users only)
+#engine = pyttsx3.init('dummy')
+engine = pyttsx3.init()
+#hazel_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-GB_HAZEL_11.0"
+#engine.setProperty('voice', hazel_voice_id)
+rate = engine.getProperty('rate')
+engine.setProperty('rate', rate-50)
+
+#------------------------------------------------
+# Step 3: Define ANSI escape sequences for text color
+colors = {
+    "blue": "\033[94m",
+    "bright_blue": "\033[96m",
+    "orange": "\033[93m",
+    "yellow": "\033[93m",
+    "white": "\033[97m",
+    "red": "\033[91m",
+    "magenta": "\033[35m",
+    "bright_magenta": "\033[95m",
+    "cyan": "\033[36m",
+    "bright_cyan": "\033[96m",
+    "green": "\033[32m",
+    "bright_green": "\033[92m",
+    "reset": "\033[0m"
+}
+
+#------------------------------------------------
+# Step 4: Ignore FP16 warnings
+warnings.filterwarnings("ignore", message="FP16 is not supported on CPU")
+
+#------------------------------------------------
+# Step 5: Point to LM Studio Local Inference Server
+openai.api_base = "http://localhost:1234/v1"
+openai.api_key = "not-needed"
+
+#------------------------------------------------
+# Step 6: Load the Whisper model
+whisper_model = whisper.load_model("tiny")  # orig=base
+
+#------------------------------------------------
+# Step 7: Define audio parameters
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 8000  # orig = 16000
+CHUNK = 1024
+SHORT_NORMALIZE = (1.0/32768.0)
+
+audio = pyaudio.PyAudio()
+lastkey = '0'
+lastpressed = False
+my_name = "Unknown"
+#------------------------------------------------
+# Step 8: Define function to detect key tuches
+def on_press(key):
+    global lastkey
+    global lastpressed
+    lastpressed = True
+    lastkey = key
+
+#------------------------------------------------
+# Step 9: Define function to speak text
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+#------------------------------------------------
+# Step 10: Define function to detect rms values
+def get_rms( block ):
+    # RMS amplitude is defined as the square root of the 
+    # mean over time of the square of the amplitude.
+    # so we need to convert this string of bytes into 
+    # a string of 16-bit samples...
+
+    # we will get one short out for each 
+    # two chars in the string.
+    count = len(block)/2
+    format = "%dh"%(count)
+    shorts = struct.unpack( format, block )
+
+    # iterate over the block.
+    sum_squares = 0.0
+    for sample in shorts:
+        # sample is a signed short in +/- 32768. 
+        # normalize it to 1.0
+        n = sample * SHORT_NORMALIZE
+        sum_squares += n*n
+
+    return math.sqrt( sum_squares / count )
+
+#------------------------------------------------
+# Get avrage rms value for background noise...
+# and time it takes to get it...
+def getNoiseLevel():
+    stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK) #, input_host_api_specific_stream_info=3)
+    rms = 0.0
+    start = time.time()
+    for i in range(10):
+        data = stream.read(CHUNK)
+        x = get_rms(data)
+        rms = rms + x
+    rms = rms/10.0
+    end = time.time()
+    print("Time: " + str((end - start) / 10.0) + " seconds/fr. and rms: " + "{:.6f}".format(rms))
+    stream.stop_stream()
+    stream.close()
+    return rms
+
+#------------------------------------------------
+# Step 11: Define function to record audio
+def record_audio(rms):
+    global lastkey
+    global lastpressed
+
+    frames = []
+    newrms = []
+
+    stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+    print(f"{colors['green']}Start speaking... (Press 'q' to stop){colors['reset']}")
+    lastdata = stream.read(CHUNK)
+    old_x = get_rms(lastdata)
+
+    # Wait for loud sound to start recording...
+    while True:
+        data = stream.read(CHUNK)
+        x = get_rms(data) 
+      #  print(str((x+old_x)/2.0)+" : " + str(rms*2)) #10))
+
+        if ((x+old_x)/2.0) > (rms*2):
+            frames.append(lastdata) # Also add frame before the loud sound...
+            frames.append(data)
+            break 
+
+        lastdata = data 
+        old_x = x
+        
+        if lastpressed == True:
+            lastpressed = False
+
+            if str(lastkey.char) == 'q':
+                return None
+
+    # Record until sound is low enough...
+    print(f"{colors['red']}Pause when done...{colors['reset']}")
+    while True:
+        data = stream.read(CHUNK)
+        frames.append(data)
+        newrms.append(get_rms(data))
+
+        # Moving average of rms...
+        if len(newrms) > 10:
+            newrms.pop(0)
+        x = 0
+        for i in newrms:
+            x = x + i
+        x=x/len(newrms)
+
+        # Check if rms is low enough to stop recording...
+        if x < rms*2:
+            break 
+
+    stream.stop_stream()
+    stream.close()
+
+#    start = time.time()
+    wf = wave.open("temp_audio.wav", 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(audio.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+ #   end = time.time()
+ #   print("Time: " + str((end - start) / 10.0) + " seconds/fr. and rms: " + "{:.6f}".format(rms))
+    return "temp_audio.wav"
+
+#------------------------------------------------
+# Step 12: Define function to get user input via GUI dialog
+def get_user_input():
+    ROOT = tk.Tk()
+    ROOT.withdraw()  # Hide the main Tkinter window
+    user_input = simpledialog.askstring(title="Text Input", prompt="Type your input:")
+    return user_input
+
+#------------------------------------------------
+# Step 12: Define function to get user input via GUI dialog
+def wait_for_silence():
+
+    newrms = []
+
+    stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+    newrms.append(get_rms(stream.read(CHUNK)))
+
+    while True:
+        newrms.append(get_rms(stream.read(CHUNK)))
+
+        # Moving average of rms...
+        if len(newrms) > 10:
+            newrms.pop(0)
+        x = 0
+        for i in newrms:
+            x = x + i
+        x=x/len(newrms)
+
+        # Check if rms is low enough to stop recording...
+        if x < rms*2:
+            break 
+
+    stream.stop_stream()
+    stream.close()
+
+#------------------------------------------------
+# Step 13: Define function to process user input and generate response
+def process_input(input_text):
+    conversation = [
+        {"role": "system", "content": "You are Mary, the assistant chatbot. As youre name is Mary you will answer questions addressed to Mary only. My name is "+my_name+", the human and user. Your role is to assist the human, who is known as "+my_name+". Respond concisely and accurately, maintaining a friendly, respectful, and professional tone. Emphasize honesty, candor, and precision in your responses."},
+        {"role": "user", "content": input_text}
+    ]
+
+    completion = client.chat.completions.create(
+        model="local-model",
+        messages=conversation,
+        temperature=0.7
+    )
+
+    assistant_reply = completion.choices[0].message.content
+    print(f"{colors['magenta']}KITT:{colors['reset']} {assistant_reply}")
+    print("I'll speak when quiet.!")
+    wait_for_silence()
+    speak(assistant_reply)
+    
+#------------------------------------------------
+# Get avrage rms value for background noise...
+def get_text():
+    audio_file = record_audio(rms)
+    if audio_file == None:
+        return None
+
+    transcribe_result = whisper_model.transcribe(audio_file)
+    transcribed_text = transcribe_result["text"]
+    os.remove(audio_file)  # Cleanup
+    return transcribed_text
+
+#------------------------------------------------
+#------------------------------------------------
+#------------------------------------------------
+
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
+
+#------------------------------------------------
+# Get avrage rms value for background noise...
+rms = getNoiseLevel()
+
+engine.say("Hello, my name is Mary, what is youre name?")
+engine.runAndWait()
+
+x = get_text()
+if x == None:
+    print("Thank you for now, hope to talk to you soon. Goodbye.")
+    engine.say("Thank you for now, hope to talk to you soon. Goodbye.")
+    engine.runAndWait()
+    print("Exiting")
+    
+else:
+    x = x.split()
+    my_name = x[len(x)-1]
+    
+    print("Hello "+my_name+", How can I assist you today?")
+    engine.say("Hello "+my_name+", How can I assist you today?")
+    engine.runAndWait()
+
+    #------------------------------------------------
+    # Step 14: Main loop to continuously monitor for user input
+    while True:
+        
+        transcribed_text = get_text()
+        if transcribed_text == None:
+            engine.say("Thank you for now, hope to talk to you soon. Goodbye.")
+            engine.runAndWait()
+            print("Exiting")
+            break
+
+        print(f"{colors['blue']}VTM:{colors['reset']} {transcribed_text}")
+        if "Mary" in transcribed_text:            
+            engine.say("So you're saying. ")
+            engine.runAndWait()
+            engine.say(transcribed_text)
+            engine.runAndWait()
+            engine.say("Let me think for a moment")
+            engine.runAndWait()
+
+            process_input(transcribed_text)
+    
+#------------------------------------------------
+# Step 15: Cleanup audio resources
+mixer.quit()
+audio.terminate()
